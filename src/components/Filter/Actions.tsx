c0ec59331148e694bbe1d54990700aaa2198f071
alikhani <alikhani.dev@gmail.com>
2022-09-14 23:28:48 +0430

    [Fix] : not work actions button

--- src/components/Filter/Actions.tsx
+++ src/components/Filter/Actions.tsx
@@ -1,18 +1,19 @@
-import React, { FC } from 'react'
-import { taskAllComplected, taskRemoveAllComplected } from '../../redux/reducer/todoSlice'
+import { FC } from 'react'
 // UI :
 import { Button } from 'react-bootstrap'
+// redux :
 import { useAppDispatch } from '../../redux'
+import { taskAllComplected, taskRemoveAllComplected } from '../../redux/reducer/todoSlice'
 
 const Actions: FC = () => {
 	const dispatch = useAppDispatch()
 
 	return (
 		<>
-			<Button className='mt-3' onClick={() => dispatch(taskRemoveAllComplected)}>
+			<Button className='mt-3' onClick={() => dispatch(taskRemoveAllComplected())}>
 				Remove All Complected
 			</Button>
-			<Button className='mt-3 ms-3 ms-sm-0' onClick={() => dispatch(taskAllComplected)}>
+			<Button className='mt-3 ms-3 ms-sm-0' onClick={() => dispatch(taskAllComplected())}>
 				All Complected
 			</Button>
 		</>
