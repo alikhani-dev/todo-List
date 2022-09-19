c077eb74f53895899e8036f9b3187ada75fcccb5
alikhani <alikhani.dev@gmail.com>
2022-09-19 20:25:24 +0430

    [Add] : preloaded state : get task on localstorage

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,7 +1,10 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
+// reducer
 import tasksSlice from './reducer/todoSlice'
 import filterSlice from './reducer/filterSlice'
+// utils :
+import { getAllTask } from './reducer/utils/localstorage'
 
 export type Color = 'secondary' | 'dark' | 'primary' | 'info' | 'success' | 'danger' | 'warning'
 export type Actions = 'All' | 'Pending' | 'Complected'
@@ -10,8 +13,12 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
 const rootReducer = { tasks: tasksSlice, filter: filterSlice }
+const store = configureStore({
+	reducer: rootReducer,
+	preloadedState: { tasks: { all: getAllTask() } },
+	devTools: process.env.NODE_ENV !== 'production'
+})
 
-const store = configureStore({ reducer: rootReducer, devTools: process.env.NODE_ENV !== 'production' })
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
